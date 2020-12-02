08fb34594d26d87bea024b15a5a788c3442c4617
Jose Jaime Figueroa Barrios <jaime.figueroa@inspiracode.net>
2020-12-01 20:39:25 -0600

    Let, Const, Destructuracion

--- src/es6/index.js
+++ src/es6/index.js
@@ -16,3 +16,29 @@ function newFunction2(name = "Jaime", age = 36, country = "MX") {
 }
 
 newFunction2();
+newFunction2("Miriamno", 36, "CO");
+
+// Let y Const
+// Destructuracion
+
+let person = {
+  name: "Jaime",
+  age: 36,
+  country: "MX",
+};
+
+// Antes:
+console.log(person.name, person.age);
+
+// Con es6+
+
+let { name, age, country } = person;
+
+console.log(name, age, country);
+
+//Arrglos
+
+let team1 = ["Jaime", "Jose", "Abraham"];
+let team2 = ["Emma", "Sara", "Miriam"];
+let education = ["Padres", ...team1, ...team2];
+console.log(education);
