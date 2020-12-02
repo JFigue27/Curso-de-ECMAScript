0d9b7a07e02b5128b7143e2c4f9d65d9aaa21355
Jose Jaime Figueroa Barrios <jaime.figueroa@inspiracode.net>
2020-12-01 21:12:03 -0600

    Arroow functons

--- src/es6/index.js
+++ src/es6/index.js
@@ -42,3 +42,51 @@ let team1 = ["Jaime", "Jose", "Abraham"];
 let team2 = ["Emma", "Sara", "Miriam"];
 let education = ["Padres", ...team1, ...team2];
 console.log(education);
+
+let nombre = "Jaime";
+let edad = 36;
+//es5
+obj = { nombre: nombre, edad: edad };
+console.log(obj);
+//es6
+obj2 = { nombre, edad };
+console.log(obj2);
+
+// Arrowfunction
+
+const names = [
+  { name: "Jaime", age: 36 },
+  { name: "Miriam", age: 37 },
+];
+
+// es5
+let listOfNames = names.map(function (item) {
+  console.log(item.name);
+});
+
+// es6
+let listOfNames2 = names.map((item) => {
+  console.log(item.name);
+});
+
+const names2 = names.map((item) => console.log(item.name));
+
+const square = (num) => num * num;
+
+// Promises
+
+const helloPromise = () => {
+  return new Promise((resolve, reject) => {
+    if (false) {
+      resolve("Hey!!!");
+    } else {
+      reject("Ups!!!");
+    }
+  });
+};
+
+helloPromise()
+  .then((res) => {
+    console.log(res);
+  })
+  .catch((reject) => console.log(reject));
