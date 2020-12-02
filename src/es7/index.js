bc4c67dd2439aafec4224fbc040c60bbb8b4caed
Jose Jaime Figueroa Barrios <jaime.figueroa@inspiracode.net>
2020-12-01 22:07:51 -0600

    ES7 whit includes and exponents

---
+++ src/es7/index.js
@@ -0,0 +1,14 @@
+// es 7 ( Includes y el la potencia)
+let numbers = [1, 2, 3, 4, 5, 7, 8];
+
+if (numbers.includes(7)) {
+  console.log("Si se encuentra el valor 7");
+} else {
+  console.log("No eta el valor 7");
+}
+
+let base = 4;
+let exponent = 3;
+let result = base ** exponent;
+
+console.log(result);
